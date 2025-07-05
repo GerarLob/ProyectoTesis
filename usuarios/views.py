7d5aac2bba16263c4249f2e0760955c106e3078e
GERARDO ALCIDES LOBOS PEREZ <globosp@miumg.edu.gt>
2025-07-04 21:00:36 -0600

    Día 7: Plantilla base creada con menú dinámico por rol y vista de bienvenida

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from .forms import CustomLoginForm
+from django.contrib.auth.decorators import login_required
 
 class CustomLoginView(LoginView):
     template_name = 'usuarios/login.html'
     authentication_form = CustomLoginForm
 
+@login_required
+def bienvenida(request):
+    return render(request, 'usuarios/bienvenida.html')
